cf932e15989c8b72b7238d0ab78cc913c90cc731
momo <liyuyu23782378@gmail.com>
2024-03-05 15:50:38 +0800

    redo lab_b02

---
+++ src/main/java/com/fsse2401/lab_b02_redo_redo/LabB02RedoRedoApplication.java
@@ -0,0 +1,13 @@
+package com.fsse2401.lab_b02_redo_redo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabB02RedoRedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabB02RedoRedoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/fsse2401/lab_b02_redo_redo/data/dto/request/CreatePersonRequestDto.java
@@ -0,0 +1,8 @@
+package com.fsse2401.lab_b02_redo_redo.data.dto.request;
+
+public class CreatePersonRequestDto {
+
+    private String lastName;
+    private String firstName;
+    private String hkid;
+}
